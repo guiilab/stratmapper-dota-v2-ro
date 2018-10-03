4442d447462bf5282ac8ae4173f3c2553d2c5021
Andy Bryant <andybryant@Andys-iMac.local>
2018-10-03 11:52:43 -0400

    reduced to 5000 events

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post('/api/events', function (req, res) {
             unit: req.body.unit,
             event_type: req.body.event_type
         })
-        .limit(40000)
+        .limit(5000)
         .exec(function (err, events) {
             return res.send(events)
         })
