886f297701a85818366ad9893907dd530e1fffdb
Andy Bryant <andybryant@Andys-iMac.local>
2018-10-03 11:47:35 -0400

    limit mdb to 50000

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post('/api/events', function (req, res) {
             unit: req.body.unit,
             event_type: req.body.event_type
         })
-        .limit(2000)
+        .limit(40000)
         .exec(function (err, events) {
             return res.send(events)
         })
