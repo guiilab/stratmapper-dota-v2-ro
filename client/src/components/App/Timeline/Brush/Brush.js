516337d652d5e20a90e0721c06d5902d9fdad5a5
Andy Bryant <andybryant@Andys-iMac.local>
2018-11-28 09:48:57 -0500

    working

--- client/src/components/App/Timeline/Brush/Brush.js
+++ client/src/components/App/Timeline/Brush/Brush.js
@@ -22,6 +22,7 @@ class Brush extends Component {
             }.bind(this), 10)
         }
         if (prevProps.brushRange[0] !== this.props.brushRange[0] && (this.context.state.playing)) {
+            console.log('hello')
             this.renderBrush()
         } else if (!this.props.zoomTransform) {
             this.updateBrush()
