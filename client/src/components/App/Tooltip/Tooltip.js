3720ab616fd5ee23708dba9c28d844ca839f385f
Andy Bryant <andybryant@dhcp-10-250-242-115.harvard.edu>
2020-01-16 08:42:20 -0500

    removed tooltip divider

--- client/src/components/App/Tooltip/Tooltip.js
+++ client/src/components/App/Tooltip/Tooltip.js
@@ -5,49 +5,48 @@ import TooltipElement from './TooltipElement/TooltipElement.js';
 import NodeContextElement from './NodeContextElement/NodeContextElement.js';
 
 class Tooltip extends Component {
-    render() {
-        const { tooltipPosition, tooltips, activeNode } = this.context.state;
+  render() {
+    const { tooltipPosition, tooltips, activeNode } = this.context.state;
 
-        let tooltipStyle;
+    let tooltipStyle;
 
-        if (tooltipPosition) {
-            tooltipStyle = {
-                opacity: 1,
-                left: tooltipPosition[0],
-                top: tooltipPosition[1]
-            }
-        } else {
-            tooltipStyle = {
-                opacity: 0
-            }
-        }
-
-        if (!activeNode) {
-            return null
-        }
-
-        return (
-            <div style={tooltipStyle} className="tooltip-container">
-                <div className="tooltipsidebar-inner-container">
-                    {tooltips[activeNode.event_type].map((element) => (
-                        <TooltipElement
-                            event={activeNode}
-                            elementKey={element.key}
-                            elementLabel={element.label}
-                            key={element.label} />)
-                    )}
-                    {activeNode.node_context ? `<div className="tooltip-divider"></div>` : null}
-                    {activeNode.node_context ? Object.keys(activeNode.node_context).map((element) => (
-                        <NodeContextElement
-                            event={activeNode}
-                            element={element}
-                            key={element}
-                        />
-                    )) : null}
-                </div>
-            </div>
-        );
+    if (tooltipPosition) {
+      tooltipStyle = {
+        opacity: 1,
+        left: tooltipPosition[0],
+        top: tooltipPosition[1]
+      }
+    } else {
+      tooltipStyle = {
+        opacity: 0
+      }
     }
+
+    if (!activeNode) {
+      return null
+    }
+
+    return (
+      <div style={tooltipStyle} className="tooltip-container">
+        <div className="tooltipsidebar-inner-container">
+          {tooltips[activeNode.event_type].map((element) => (
+            <TooltipElement
+              event={activeNode}
+              elementKey={element.key}
+              elementLabel={element.label}
+              key={element.label} />)
+          )}
+          {activeNode.node_context ? Object.keys(activeNode.node_context).map((element) => (
+            <NodeContextElement
+              event={activeNode}
+              element={element}
+              key={element}
+            />
+          )) : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 Tooltip.contextType = Context;
