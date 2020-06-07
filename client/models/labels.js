5ac650f05d05090255b7d207ce91b41547cd0787
Andy Bryant <a.mcdermid.bryant@gmail.com>
2020-06-07 11:40:29 -0400

    updated label collection name

--- client/models/labels.js
+++ client/models/labels.js
@@ -3,38 +3,38 @@ const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var LabelSchema = new Schema({
-    id: {
-        type: String,
-        required: true
-    },
-    title: {
-        type: String,
-        required: false
-    },
-    author: {
-        type: String,
-        required: false
-    },
-    match: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    event_ids: {
-        type: Array,
-        required: false
-    },
-    units: {
-        type: Array,
-        required: false
-    },
-    events: {
-        type: Array,
-        required: false
-    }
+  id: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: false
+  },
+  author: {
+    type: String,
+    required: false
+  },
+  match: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  event_ids: {
+    type: Array,
+    required: false
+  },
+  units: {
+    type: Array,
+    required: false
+  },
+  events: {
+    type: Array,
+    required: false
+  }
 }, { strict: false });
 
-module.exports = mongoose.model('Label', LabelSchema);
+module.exports = mongoose.model('Label', LabelSchema, 'labels_dota_prod');
